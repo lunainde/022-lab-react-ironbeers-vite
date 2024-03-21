e1f389ecd52c0910435d3b72ae1105991338db25
lunainde <lunainde@gmail.com>
2024-03-21 15:20:06 +0100

    iteration 1-3, pages/navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,16 @@
-function Navbar() {}
+// src/components/Navbar.jsx
+
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+    return (
+      <nav>
+        <Link to="/" className="home-icon">
+            <i className="fas fa-home"></i>
+        </Link>
+      </nav>
+    )
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,36 @@
-function HomePage() {}
+// src/pages/HomePage.jsx
 
+import {Link} from "react-router-dom"
+import Navbar from "../components/Navbar";
+
+
+function HomePage() {
+    return(
+      <div>
+        <div className="card-container">
+            <Link to="/beers">
+                <img className="card-image" src="/src/assets/beers.png" alt="All Beers"/>
+                <div className="card-content">
+                <h3>ALL BEERS</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe similique placeat exercitationem necessitatibus, accusantium cupiditate iusto labore adipisci quos magni. Aliquam iusto assumenda molestias quod voluptatem quasi corporis nihil quibusdam.</p>
+                </div>
+            </Link>
+            <Link to="/beers">
+                <img className="card-image" src="/src/assets/random-beer.png" alt="All Beers"/>
+                <div className="card-content">
+                <h3>RANDOM BEER</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe similique placeat exercitationem necessitatibus, accusantium cupiditate iusto labore adipisci quos magni. Aliquam iusto assumenda molestias quod voluptatem quasi corporis nihil quibusdam.</p>
+                </div>
+            </Link>
+            <Link to="/beers">
+                <img className="card-image" src="/src/assets/new-beer.png" alt="All Beers"/>
+                <div className="card-content">
+                <h3>NEW BEER</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe similique placeat exercitationem necessitatibus, accusantium cupiditate iusto labore adipisci quos magni. Aliquam iusto assumenda molestias quod voluptatem quasi corporis nihil quibusdam.</p>
+                </div>
+            </Link>
+        </div>
+      </div>
+    )
+}
 export default HomePage;
